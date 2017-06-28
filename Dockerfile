bfbb76f32c12d8744e795225800695126b70dd79
Lee <dev@azelf.com>
2017-06-28 22:11:14 +0900

    First commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM        azelf/greenwrap
+MAINTAINER  dev@azelf.com
+
+# DJANGO_SETTINGS_MODULE설정
+COPY        . /srv/app
+WORKDIR     /srv/app/django_app
+CMD         supervisord -n
+EXPOSE      80 8000
---
+++ django_app/config/settings/debug.py
@@ -0,0 +1,40 @@
+# debug.py
+from .base import *
+
+config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
+
+# INSTALLED_APPS
+INSTALLED_APPS.append('django_extensions')
+
+# WSGI application
+WSGI_APPLICATION = 'config.wsgi.debug.application'
+
+# Static URLs
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+
+# 디버그모드니까 DEBUG는 True
+DEBUG = True
+ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Celery
+CELERY_BROKER_URL = '{}:{}'.format(
+    config_secret_debug['django']['celery']['broker_url'],
+    config_secret_debug['django']['celery']['broker_port']
+)
+CELERY_RESULT_BACKEND = '{}:{}'.format(
+    config_secret_debug['django']['celery']['broker_url'],
+    config_secret_debug['django']['celery']['broker_port']
+)
+print('@@@@@@ DEBUG:', DEBUG)
+print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
